ef058b2c06ca9c969086c6b0f45827d6e18258d4
TasseDeCafe <44320355+TasseDeCafe@users.noreply.github.com>
2026-05-30 12:38:36 +0200

    style(backend): apply eslint auto-fixes

--- apps/backend/src/transport/third-party/stripe/create-billing-portal-url/mock-create-billing-portal-url.ts
+++ apps/backend/src/transport/third-party/stripe/create-billing-portal-url/mock-create-billing-portal-url.ts
@@ -1,7 +1,6 @@
 export const mockCreateBillingPortalUrl = async (
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   customerId: string,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   returnUrl: string
 ): Promise<string> => {
   return 'some-billing-portal-url'
--- apps/backend/src/transport/third-party/stripe/list-subscriptions/mock-list-all-subscriptions.ts
+++ apps/backend/src/transport/third-party/stripe/list-subscriptions/mock-list-all-subscriptions.ts
@@ -1,9 +1,6 @@
 import { ListStripeSubscriptionsResponse } from '../stripe-api'
 
-export const mockListAllSubscriptions = async (
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  customerId: string
-): Promise<ListStripeSubscriptionsResponse> => {
+export const mockListAllSubscriptions = async (customerId: string): Promise<ListStripeSubscriptionsResponse> => {
   return [
     {
       id: 'sub_free_trial_id',
